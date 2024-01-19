d3b5b557d8dbfd5165c9aab6736ae2d46639e327
shivkumarojha <ojhashivkumar88@gmail.com>
2024-01-19 16:24:27 +0530

    Week 1: easy: times.js assignment done

--- week-1/Week-1-assignment-with-tests/01-js/medium/times.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/times.js
@@ -8,5 +8,13 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = new Date().getTime( )
+    let sum = 0
+    for(let i=1; i<=n; i++) {
+        sum += 1
+    }
+    let endTime = new Date().getTime()
+    console.log((endTime - startTime)/1000)
 }
+
+calculateTime(10888023433)
