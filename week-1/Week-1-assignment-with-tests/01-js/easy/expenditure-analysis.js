224f1d662b99d3e2317d4e94de38d127a1ad1fa7
shivkumarojha <ojhashivkumar88@gmail.com>
2024-01-19 14:40:11 +0530

    Week1:easy: expenditure assignment done

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -8,8 +8,70 @@
   - `npm run test-expenditure-analysis`
 */
 
+let transactions = [
+  {
+    itemName: "Coke",
+    category: "Drink",
+    price: 40,
+    timestamp: 43432,
+  },
+  {
+    itemName: "Honey",
+    category: "Eat",
+    price: 340,
+    timestamp: 443432,
+  },
+  {
+    itemName: "Vegetables",
+    category: "Eat",
+    price: 20,
+    timestamp: 22,
+  },
+  {
+    itemName: "Milk",
+    category: "Drink",
+    price: 70,
+    timestamp: 434,
+  },
+  {
+    itemName: "Detol",
+    category: "LifeStyle",
+    price: 40,
+    timestamp: 853452,
+  },
+  {
+    itemName: "rwol",
+    category: "LifeStyle",
+    price: 20,
+    timestamp: 853452,
+  },
+];
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let spendEstimates = {}
+  for(let i=0; i<transactions.length; i++) {
+    let t = transactions[i]
+    if(spendEstimates[t.category]) {
+      spendEstimates[t.category] = spendEstimates[t.category] + t.price
+    }else {
+      spendEstimates[t.category] = t.price
+    }
+  }
+
+  let key = Object.keys(spendEstimates)
+  let structuredSpentEstimate = []
+  for(let i=0; i<key.length; i++) {
+    let obj = {
+      category: key[i],
+      totalSpent: spendEstimates[key[i]]
+    }
+    structuredSpentEstimate.push(obj)
+  }
+
+  return structuredSpentEstimate
+
 }
 
+let result = calculateTotalSpentByCategory(transactions)
+console.log(result)
 module.exports = calculateTotalSpentByCategory;
