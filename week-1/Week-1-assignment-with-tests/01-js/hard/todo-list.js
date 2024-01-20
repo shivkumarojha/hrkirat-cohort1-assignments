c0f047e1809eb735642cd20ce2374366c6955252
shivkumarojha <ojhashivkumar88@gmail.com>
2024-01-20 13:33:22 +0530

    week1 :01 hard assignment done

--- week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -12,7 +12,69 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = []
+  }
 
+  // Add todo to the todos array
+  add(todo) {
+    this.todos.push(todo)
+    console.log(this.todos)
+  }
+
+  // Remove todo at specified index
+  remove(indexOfTodo) {
+    this.todos.splice(indexOfTodo, 1)
+    
+  }
+
+  // Update todo
+  update(index, updateTodo) {
+    let lengthOfTodos = this.todos.length
+    if(index >= lengthOfTodos || index < 0) {
+      console.log("Index is out of range")
+    } else {
+      this.todos[index] = updateTodo
+    }
+  }
+
+  // Get todo at certain index
+  get(indexOfTodo) {
+    let lengthOfTodos = this.todos.length
+    if (indexOfTodo >= lengthOfTodos || indexOfTodo < 0) {
+      console.log("Index is out of range")
+      return null
+    }else {
+      return this.todos[indexOfTodo]
+    }
+  }
+
+  // Get all todos 
+  getAll() {
+    return this.todos
+  }
+
+  // Clear all todos
+  clear() {
+    this.todos = []
+  }
 }
 
+let todos = new Todo()
+todos.add("first todo")
+todos.add("second todo")
+todos.add("third todo")
+
+todos.update(1, "Hello world")
+console.log(todos.getAll())
+
+
+todos.remove(0)
+console.log(todos.getAll())
+
+specificTodo = todos.get(1)
+console.log(specificTodo)
+
+todos.clear()
+console.log(todos.getAll())
 module.exports = Todo;
