53072d8bb63be50c553dba088a4673d86a0edbdd
shivkumarojha <ojhashivkumar88@gmail.com>
2024-01-20 12:46:57 +0530

    week 1: hard: calculater assignment is done but one test case failing divide by zero

--- week-1/Week-1-assignment-with-tests/01-js/hard/calculator.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/calculator.js
@@ -7,7 +7,7 @@
     - divide: takes a number and divide it to the result
     - clear: makes the `result` variable to 0
     - getResult: returns the value of `result` variable
-    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
+    - calc: takes a string expression which can take multi-arithmetic operations and give its result
       example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
       Points to Note: 
         1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
@@ -17,6 +17,88 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  constructor () {
+    this.result = 0
+  }
+  add(number) {
+    this.result += number
+    console.log(this.result)
+    return this.result
+  }
+  subtract(number) {
+    this.result -= number
+    console.log("substracted ", this.result)
+  }
+  
+  multiply(number) {
+    this.result *= number
+    console.log("Multiplied", this.result)
+  }
+  
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Division by  zero is not allowed")
+    }else {
+      this.result /= number
+      console.log("Divided result ", this.result)
+    }
+  }
+// Sanitizing the expression for removing spaces and other string
+sanitizeExpression(expressionString) {
+  let sanitizedString = ""
+  for(let i=0; i< expressionString.length; i++) {
+    if(expressionString[i] === " ") {
 
+    } else {
+      sanitizedString += expressionString[i]
+    }
+  }
+  console.log(sanitizedString)
+  return sanitizedString
+}
+
+  // calulating expression
+  calculate(expression) {
+    let expressionString = expression;
+    let sanitizedExpressionString = this.sanitizeExpression(expressionString);
+    // Validate input for invalid characters
+    if (!/^[0-9+\-*/().\s]+$/.test(sanitizedExpressionString)) {
+      throw new Error("Invalid characters in the expression");
+    }
+    for(let i=0; i< sanitizedExpressionString.length; i++) {
+      if (
+        sanitizedExpressionString[i] === "/" &&
+        sanitizedExpressionString[i + 1] === 0
+      ) {
+        console.log("yes found");
+        throw new Error("Divide by Zero Error");
+      } 
+    }
+      this.result = eval(sanitizedExpressionString);
+      console.log("calculate function result ", this.result);
+    
+    
+  }
+
+
+  getResult() {
+    return this.result
+  }
+  clear() {
+    this.result = 0
+  }
+}
+
+
+let calc = new Calculator()
+calc.add(10)
+calc.add(218);
+calc.add(3);
+
+calc.subtract(5)
+calc.multiply(2)
+calc.divide(10)
+// calc.divide(0)
+calc.calculate("10/0 +   2 *    (   6 - (4 + 1) / 2) + 7");
 module.exports = Calculator;
