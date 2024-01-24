e0e17f061797582262e890fcd410d61c0dd9659f
shivkumarojha <ojhashivkumar88@gmail.com>
2024-01-24 23:06:47 +0530

    week 3 04-hard assignment added mongoose config for database integration in course app

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose')
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+// Define Mongoose schema
+const userSchema = new mongoose.Schema({
+  username: {type: String},
+  password: String,
+  purchasedCourses: [{ type: 'ObjectId', ref:'Course'}]
+})
 
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String
+})
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+  published: Boolean
+})
+
+// Define Mongoose model
+const User = mongoose.model('User', userSchema)
+const Admin = mongoose.model('Admin', adminSchema)
+const Course = mongoose.model('Course', courseSchema)
+
+// Connect to mongodb cluster
+mongoose.connect('mongodb+srv://shivkumarojha:shiva%40321%23@cluster0.0xxo9rh.mongodb.net/courses')
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+  
 });
 
 app.post('/admin/login', (req, res) => {
