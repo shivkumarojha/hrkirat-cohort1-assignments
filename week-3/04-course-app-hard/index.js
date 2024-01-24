1d2e4a219c039708fffd622ea2ba2d6a3bfa11d9
shivkumarojha <ojhashivkumar88@gmail.com>
2024-01-24 23:23:11 +0530

    04 hard assignment admin signup route done

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken')
+
 const mongoose = require('mongoose')
 app.use(express.json());
 
+
+// Secret key for admin
+const secretForAdmin = "123_gili_gili_chhu";
+
+// Generate Jwt token for admin
+const generateJwtTokenAdmin = (username) => {
+  const token = jwt.sign({ username: username }, secretForAdmin, {
+    expiresIn: "1h",
+  });
+  return token;
+};
+
+// Generate web token for user
+const secretForUser = "Saka_laka_boom_boom";
+const generateJwtTokenUser = (username) => {
+  const token = jwt.sign({ username: username }, secretForUser, {
+    expiresIn: "1h",
+  });
+  return token;
+};
 // Define Mongoose schema
 const userSchema = new mongoose.Schema({
   username: {type: String},
@@ -31,9 +53,24 @@ const Course = mongoose.model('Course', courseSchema)
 // Connect to mongodb cluster
 mongoose.connect('mongodb+srv://shivkumarojha:shiva%40321%23@cluster0.0xxo9rh.mongodb.net/courses')
 // Admin routes
-app.post('/admin/signup', (req, res) => {
-  
+app.post('/admin/signup', async(req, res) => {
+  const { username, password } = req.body;
+  const adminExist = await Admin.findOne({ username })
+  if (adminExist) {
+    return res.status(403).json({ message: "Admin already exists!" });
+  } else {
+    const newAdmin =  new Admin({
+      username, password
+    })
+    newAdmin.save()
+    const token = generateJwtTokenAdmin(username);
+    res.status(200).json({
+      message: "Admin created successfully",
+      token: token,
+    });
+  }
 });
+
 
 app.post('/admin/login', (req, res) => {
   // logic to log in admin
